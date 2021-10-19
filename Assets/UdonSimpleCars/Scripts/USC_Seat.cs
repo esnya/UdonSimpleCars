1084c77c471a860fe6449bdd45fb86695f20a49c
esnya <esnya@nekometer.com>
2021-10-19 12:08:13 +0900

    :tada: feat: Initial commit

---
+++ Assets/UdonSimpleCars/Scripts/USC_Seat.cs
@@ -0,0 +1,58 @@
+﻿
+using UdonSharp;
+using UdonToolkit;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace UdonSimpleCars
+{
+    [RequireComponent(typeof(VRCStation))]
+    [RequireComponent(typeof(Collider))]
+    [DefaultExecutionOrder(1000)] // After USC_Car
+    [UdonBehaviourSyncMode(BehaviourSyncMode.NoVariableSync)]
+    public class USC_Seat : UdonSharpBehaviour
+    {
+        [Popup("GetButtonList")] public string getOutButton = "Oculus_CrossPlatform_Button4";
+        public KeyCode getOutKey = KeyCode.Return;
+
+        private USC_Car car;
+        private VRCStation station;
+        private void Start()
+        {
+            car = GetComponentInParent<USC_Car>();
+            station = (VRCStation)GetComponent(typeof(VRCStation));
+        }
+
+        private void Update()
+        {
+            if (Input.GetKey(getOutKey) || Input.GetButton(getOutButton))
+            {
+                station.ExitStation(Networking.LocalPlayer);
+            }
+        }
+
+        public override void Interact()
+        {
+            Networking.LocalPlayer.UseAttachedStation();
+        }
+
+        public override void OnStationEntered(VRCPlayerApi player)
+        {
+            if (player.isLocal) car._GetIn();
+        }
+
+        public override void OnStationExited(VRCPlayerApi player)
+        {
+            if (player.isLocal) car._GetOut();
+        }
+
+#if !COMPILER_UDONSHARP && UNITY_EDITOR
+        private string[] GetButtonList() => new[] {
+            "Oculus_CrossPlatform_PrimaryThumbstick",
+            "Oculus_CrossPlatform_SecondaryThumbstick",
+            "Oculus_CrossPlatform_Button4",
+            "Oculus_CrossPlatform_Button2",
+        };
+#endif
+    }
+}
