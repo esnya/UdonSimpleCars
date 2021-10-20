40b12575b774793f8bc844d369c95de48fc14847
esnya <esnya@nekometer.com>
2021-10-20 15:13:52 +0900

    :recycle: refactor(tug): Seperate TugJoint/TugAnchor

---
+++ Assets/UdonSimpleCars/Scripts/USC_TugAnchor.cs
@@ -0,0 +1,33 @@
+
+using UdonSharp;
+using UnityEngine;
+using VRC.SDK3.Components;
+using VRC.SDKBase;
+using VRC.Udon;
+using VRC.Udon.Common.Interfaces;
+
+namespace UdonSimpleCars
+{
+    [
+        UdonBehaviourSyncMode(/*BehaviourSyncMode.None*/ BehaviourSyncMode.NoVariableSync),
+    ]
+    public class USC_TugAnchor : UdonSharpBehaviour
+    {
+        [Tooltip("Default: VRCObjectSync in parent")] public GameObject ownerDetector;
+        [HideInInspector] public Rigidbody vehicleRigidbody;
+
+        private void Start()
+        {
+            var objectSync = (VRCObjectSync)GetComponentInParent(typeof(VRCObjectSync));
+            if (ownerDetector == null && objectSync != null) ownerDetector = objectSync.gameObject;
+
+            vehicleRigidbody = objectSync != null ? objectSync.GetComponent<Rigidbody>() : null;
+            if (vehicleRigidbody == null) vehicleRigidbody = GetComponentInParent<Rigidbody>();
+        }
+
+        public bool _Connectable()
+        {
+            return Networking.IsOwner(ownerDetector) && !vehicleRigidbody.isKinematic;
+        }
+    }
+}
