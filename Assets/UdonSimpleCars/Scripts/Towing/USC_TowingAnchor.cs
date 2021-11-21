900f05e5e765cdf02adfd1859978469623aae613
esnya <esnya@nekometer.com>
2021-11-21 18:20:14 +0900

    :sparkles: feat(towing): Auto detect OwnerDetector from SaccFlight vehicles

--- Assets/UdonSimpleCars/Scripts/Towing/USC_TowingAnchor.cs
+++ Assets/UdonSimpleCars/Scripts/Towing/USC_TowingAnchor.cs
@@ -3,6 +3,7 @@ using System;
 using UdonSharp;
 using UnityEngine;
 using VRC.SDK3.Components;
+using VRC.Udon;
 
 namespace UdonSimpleCars
 {
@@ -10,7 +11,7 @@ namespace UdonSimpleCars
     public class USC_TowingAnchor : UdonSharpBehaviour
     {
         #region Public Variables
-        [Tooltip("Default: VRCObjectSync in parent")]
+        [Tooltip("Default: VRCObjectSync in parent or find from SaccFlightAndVehicles")]
         public GameObject ownerDetector;
         #endregion
 
@@ -23,12 +24,39 @@ namespace UdonSimpleCars
         private void Start()
         {
             var objectSync = (VRCObjectSync)GetComponentInParent(typeof(VRCObjectSync));
-            if (ownerDetector == null && objectSync != null) ownerDetector = objectSync.gameObject;
+            if (objectSync)
+            {
+                vehicleRigidbody = objectSync.GetComponent<Rigidbody>();
+            }
+            if (!ownerDetector) ownerDetector = FindOwnerDetector();
+            if (!vehicleRigidbody) vehicleRigidbody = transform.parent.GetComponentInParent<Rigidbody>();
 
-            if (objectSync != null) vehicleRigidbody = objectSync.GetComponent<Rigidbody>();
-            if (vehicleRigidbody == null) vehicleRigidbody = transform.parent.GetComponentInParent<Rigidbody>();
+            Debug.Log($"{this} {ownerDetector}");
 
             attachedWheelCollider = GetComponentInParent<WheelCollider>();
+        }
+        #endregion
+
+        #region Internal Logics
+        private GameObject FindOwnerDetector()
+        {
+            var objectSync = (VRCObjectSync)GetComponentInParent(typeof(VRCObjectSync));
+            if (objectSync) return objectSync.gameObject;
+
+            var rigidbody = transform.parent.GetComponentInParent<Rigidbody>();
+            if (rigidbody)
+            {
+                foreach (var udon in rigidbody.GetComponentsInChildren(typeof(UdonBehaviour)))
+                {
+                    var usharp = (UdonSharpBehaviour)udon;
+                    if (!usharp) continue;
+                    var name = usharp.GetUdonTypeName();
+                    if (name == "SaccAirVehicle" || name == "SaccSeaVehicle" || name == "EngineController" || name == "SyncScript") return udon.gameObject;
+                }
+                return rigidbody.gameObject;
+            }
+
+            return gameObject;
         }
         #endregion
     }
